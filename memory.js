a58f981e365a661cddc5ed917a0201f4301161e5
trista <trista_mccleary@yahoo.com>
2016-11-11 15:07:30 -0500

    initial state changes implemented

--- memory.js
+++ memory.js
@@ -4,14 +4,34 @@ app.controller('MemoryController', function($scope) {
   var card2 = new Card("images/monsters-02.png", false, false);
   var card3 = new Card("images/monsters-03.png", false, false);
   var card4 = new Card("images/monsters-04.png", false, false);
-  var card5 = new Card("images/monsters-05.png", false, false);
-  var card6 = new Card("images/monsters-06.png", false, false);
-  var card7 = new Card("images/monsters-07.png", false, false);
-  var card8 = new Card("images/monsters-08.png", false, false);
+  var card5 = new Card("images/monsters-01.png", false, false);
+  var card6 = new Card("images/monsters-02.png", false, false);
+  var card7 = new Card("images/monsters-03.png", false, false);
+  var card8 = new Card("images/monsters-04.png", false, false);
 
   $scope.grid = [
     [card1, card2, card3, card4], [card5, card6, card7, card8]
   ];
+
+  $scope.state = "first";
+
+  $scope.flip = function(card) {
+    if ($scope.state === "first") {
+      card.open = true;
+      $scope.firstCard = card;
+      $scope.state = "second";
+    } else {
+      card.open = true;
+      if (card.url === $scope.firstCard.url) {
+        card.matched = true;
+        $scope.firstCard.matched = true;
+      }
+    }
+
+  };
+
+  $scope.firstCard = null;
+
 });
 
 function Card(url, open, matched) {
