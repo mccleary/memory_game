66af2d32072bb127c1a6aab371b47baa32711592
trista <trista_mccleary@yahoo.com>
2016-11-15 14:17:03 -0500

    basic game now playable with multiple levels

--- memory.js
+++ memory.js
@@ -1,19 +1,65 @@
+function Card(monsterNum, open, matched) {
+  this.url = 'images/monsters-' + monsterNum + '.png';
+  this.open = open;
+  this.matched = matched;
+}
+
+
 var app = angular.module('memory', []);
 app.controller('MemoryController', function($scope, $timeout) {
+  // variable to display difficulty selection menu
+  $scope.menu = true;
+  // holds randomly selected monster (img number 1 - 16)
   $scope.monsters = [];
+  // number of unique monsters in a game
+  $scope.uniqueMonsters = 16;
+  // generated monster cards to be passed to the rows
+  $scope.monsterCards = [];
+  // number of rows (varies depending on game difficulty)
+  $scope.numRows = 4;
+  // number of monster cards in each row
+  $scope.monsterCardsPerRow = 8;
+
+  $scope.gameMode = function(str) {
+    if (str === 'easy') {
+      $scope.uniqueMonsters = 4;
+      $scope.numRows = 2;
+      $scope.monsterCardsPerRow = 4;
+    }
+    else if (str === 'medium') {
+      $scope.uniqueMonsters = 9;
+      $scope.numRows = 3;
+      $scope.monsterCardsPerRow = 6;
+    }
+    else {
+      $scope.uniqueMonsters = 16;
+      $scope.numRows = 4;
+      $scope.monsterCardsPerRow = 8;
+    }
+    $scope.menu = false;
+    $scope.generateGame();
+  };
+
 
   $scope.monsterGenerator = function() {
-    while ($scope.monsters.length < 8) {
+    // adds a unique, random monster to the monster array twice (so it can be used to make two matching cards)
+    while ($scope.monsters.length < $scope.uniqueMonsters * 2) {
       var num = $scope.random();
       if ($scope.monsters.indexOf(num) === -1) {
         $scope.monsters.push(num);
         $scope.monsters.push(num);
-
       }
     }
+    console.log($scope.monsters);
+  };
+
+  $scope.random = function() {
+    // there are 16 unique monsters
+    return Math.floor(Math.random() * 16) + 1;
   };
 
   $scope.shuffle = function(arr) {
+    // used to shuffle the monster array before creating the cards
     var i = 0,
         j = 0,
         temp = null;
@@ -25,36 +71,27 @@ app.controller('MemoryController', function($scope, $timeout) {
     }
   };
 
-
-
-  $scope.random = function() {
-    return Math.floor(Math.random() * 16) + 1;
+  $scope.generateGame = function() {
+    // main game board
+    $scope.grid = [];
+    // generate monsters and shuffle their order
+    $scope.monsterGenerator();
+    $scope.shuffle($scope.monsters);
+    // populate game board dynamically
+    for (var i = 0; i < $scope.uniqueMonsters * 2; i++) {
+      $scope.monsterCards.push(new Card($scope.monsters[i], false, false));
+    }
+    for (var i = 0; i < $scope.numRows; i++) {
+      $scope.grid.push([]);
+      for (var j = 0; j < $scope.monsterCardsPerRow; j++) {
+        $scope.grid[i].push($scope.monsterCards.pop());
+      }
+    }
   };
-  $scope.monsterGenerator();
-  $scope.shuffle($scope.monsters);
-
-  $scope.cards = [];
-
-  for (var i = 0; i < 8; i++) {
-    var random = Math.floor(Math.random() * 4);
-    $scope.cards[i] = new Card(monsters[random], false, false);
-  }
-
-  var card1 = new Card($scope.monsters[0], false, false);
-  var card2 = new Card($scope.monsters[1], false, false);
-  var card3 = new Card($scope.monsters[2], false, false);
-  var card4 = new Card($scope.monsters[3], false, false);
-  var card5 = new Card($scope.monsters[4], false, false);
-  var card6 = new Card($scope.monsters[5], false, false);
-  var card7 = new Card($scope.monsters[6], false, false);
-  var card8 = new Card($scope.monsters[7], false, false);
-
-  $scope.grid = [
-    [card1, card2, card3, card4], [card5, card6, card7, card8]
-  ];
 
   $scope.state = "first";
   $scope.firstCard = null;
+  $scope.isDisabled = false;
 
   $scope.flip = function(card) {
     if ($scope.state === "first") {
@@ -69,13 +106,18 @@ app.controller('MemoryController', function($scope, $timeout) {
         $scope.state = "first";
       } else {
         $scope.state = "first";
+        $scope.isDisabled = true;
         $timeout(function() {
           card.open = false;
           $scope.firstCard.open = false;
-        }, 500);
+          $scope.isDisabled = false;
+        }, 1000);
       }
       if ($scope.checkWin($scope.grid)) {
         $scope.message = "You win!";
+        $timeout(function() {
+          $scope.menu = true;
+        }, 2000);
       }
     }
   };
@@ -90,13 +132,3 @@ app.controller('MemoryController', function($scope, $timeout) {
 
 
 });
-
-function Card(monsterNum, open, matched) {
-  this.url = 'images/monsters-' + monsterNum + '.png';
-  this.open = open;
-  this.matched = matched;
-}
-
-function monsterGenerator() {
-
-}
